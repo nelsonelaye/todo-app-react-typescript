f5050b20d0372d09663c4d5ec14f83eb3307c7bd
nelsonelaye <nelsonelaye@gmail.com>
2022-12-02 10:09:03 +0100

    Finished updating ErrorBoundary

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { ErrorInfo, ReactNode } from "react";
 
-type states = {
+type MyProps = {
+  children: ReactNode;
+};
+type States = {
   hasError: boolean;
 };
-class ErrorBoundary extends React.Component {
-  constructor(props: any) {
+class ErrorBoundary extends React.Component<MyProps, States> {
+  constructor(props: MyProps) {
     super(props);
-    this.state = {
-      hasError: false,
-    };
   }
 
-  static getDerivedStateFromError(error: any) {
+  state: States = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(error: Error): States {
     return { hasError: true };
   }
 
-  componentDidCatch(error: any, info: any) {
+  public componentDidCatch(error: Error, info: ErrorInfo) {
     console.log("Error", error);
     console.log("Info", info);
   }
   render(): React.ReactNode {
-    // if (this.state.hasError) {
-    return <h1>ErrorBoundary</h1>;
-    // }
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Something went wrong.</h1>;
+          <p>It's not you but us, please hold on while we fix this</p>
+          <button
+            type="button"
+            style={{
+              padding: "20px",
+              outline: "none",
+              border: 0,
+              borderRadius: "10px",
+            }}
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            Reload Page
+          </button>
+        </div>
+      );
+    }
+
+    return this.props.children;
   }
 }
 
